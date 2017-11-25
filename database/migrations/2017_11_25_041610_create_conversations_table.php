3742db3323f3a491341f6f4efcf50e5ff3e788a5
shield <myshield02@gmail.com>
2017-11-25 18:58:43 +0100

    commit

---
+++ database/migrations/2017_11_25_041610_create_conversations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateConversationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('conversations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('gender');
+            $table->string('profile_pix');
+            $table->string('messenger_uid');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('conversations');
+    }
+}
---
+++ resources/views/conversation.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Conversation History</title>
+
+    <!--style-->
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+    <div class="container reset">
+        <aside class="list reset">
+            <ul class="persons reset">
+                <li>chat 1</li>
+                <li>chat 2</li>
+                <li>chat 3</li>
+                <li>chat 4</li>
+                <li>chat 5</li>
+                <li>chat 6</li>
+                <li>chat 6</li>
+                <li>chat 6</li>
+                <li>chat 6</li>
+                <li>chat 6</li>
+                <li>chat 6</li>
+                <li>chat 6</li>
+                <li>chat 6</li>
+                <li>chat 6</li>
+                <li>chat 6</li>
+                <li>chat 6</li>
+                <li>chat 6</li>
+                <li>chat 6</li>
+                <li>chat 6</li>
+                <li>chat 6</li>
+                <li>chat 6</li>
+                <li>chat 6</li>
+            </ul>
+        </aside>
+        <section class="chat reset">
+            <div class="caption reset">
+                <h1>Conversation History</h1>
+                <h3>first name, last name</h3>
+            </div>
+        </section>
+        <footer class="info reset">
+
+        </footer>
+    </div>
+</body>
+</html>
