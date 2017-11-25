b7760dcf640708a72f9e48fc0a5bf54500017bbc
shield <myshield02@gmail.com>
2017-11-25 18:55:10 +0100

    commit

---
+++ app/Http/Controllers/Conversation.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Conversation extends Controller
+{
+    //
+
+    public function __constructor() {
+
+    }
+
+    public function date() {
+        return response()->json([
+            'message' => [
+                'text' => "Today's date is "
+            ]
+        ]);
+    }
+}
