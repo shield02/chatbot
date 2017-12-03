a13aa7f23035d24619190ad1b8f0faa03ea92955
shield <myshield02@gmail.com>
2017-12-03 05:01:25 +0100

    Conversation history update

---
+++ app/Http/Controllers/ConversationHistory.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Conversation;
+
+use Illuminate\Http\Request;
+
+class ConversationHistory extends Controller
+{
+    //
+
+    public function __constructor() {
+
+    }
+}
