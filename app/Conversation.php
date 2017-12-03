94dbadba91417e8ea58adb0757cc7f0412833fd3
shield <myshield02@gmail.com>
2017-12-03 04:58:57 +0100

    Model update

--- app/Conversation.php
+++ app/Conversation.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Conversation extends Model
 {
-    //
+    /**
+     * Mass assignable attributes
+     * 
+     * @var array
+     */
+
+     protected $fillable = ['chatid', 'first_name', 'last_name', 'profile_pix'];
 }
